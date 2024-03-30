9037e2361eeca581566fbdb3350f5f1e5f1d23c7
fbifbi212 <fbifbi212@gmail.com>
2024-03-30 18:22:44 +0300

    end point update

--- src/app/admin-panel/data.service.ts
+++ src/app/admin-panel/data.service.ts
@@ -9,14 +9,19 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getItems(): Observable<any[]> {
-    return this.http.get<any[]>('http://localhost:5214/api/arac');
+    return this.http.get<any[]>('http://api.hangarautos.com/api/arac');
   }
 
   editArac(aracId: number, arac: any): Observable<any> {
-    return this.http.put<any>(`http://localhost:5214/api/arac/${aracId}`, arac);
+    return this.http.put<any>(
+      `http://api.hangarautos.com/api/arac/${aracId}`,
+      arac
+    );
   }
 
   deleteArac(aracId: number): Observable<any> {
-    return this.http.delete<any>(`http://localhost:5214/api/arac/${aracId}`);
+    return this.http.delete<any>(
+      `http://api.hangarautos.com/api/arac/${aracId}`
+    );
   }
 }
